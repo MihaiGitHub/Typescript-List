c14c8f004d5f75ddaf5fd35f58134ec36685b328
Mihai Smarandache <mihai.sanfran@gmail.com>
2022-09-23 18:51:32 -0700

    Create data interface

--- index.js
+++ index.js
@@ -3,5 +3,10 @@ exports.__esModule = true;
 var axios_1 = require("axios");
 var url = "https://jsonplaceholder.typicode.com/todos/1";
 axios_1["default"].get(url).then(function (response) {
-    console.log(response.data);
+    // tell typescript what type of data this is
+    var todo = response.data;
+    var id = todo.id;
+    var title = todo.title;
+    var finished = todo.completed;
+    console.log("\n        The TO DO with ID: ".concat(id, "\n        Has a title of: ").concat(title, "\n        Is it finished? ").concat(finished, "\n    "));
 });
